48ead925c73d59b3fe30309be6e5826078e5cb86
Pragati Deshpande <142827562+Pragati790@users.noreply.github.com>
2025-02-13 23:24:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Website Loaded Successfully");
 });
+  <!-- JavaScript for Toggle Menu -->
+    const menuToggle = document.getElementById("menuToggle");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    menuToggle.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
