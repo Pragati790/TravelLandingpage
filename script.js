6a4ac08d4bcb23537974f67af2af65c6104ef16d
Pragati Deshpande <142827562+Pragati790@users.noreply.github.com>
2025-02-14 14:56:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Website Loaded Successfully");
 });
- 
-     const menuToggle = document.getElementById('menuToggle');
-    const mobileMenu = document.getElementById('mobileMenu');
 
-    menuToggle.addEventListener('click', () => {
-        mobileMenu.classList.toggle('mobile-menu-open');
-    });
 
-    // Close menu when clicking a link
-    document.querySelectorAll('#mobileMenu a').forEach(link => {
-        link.addEventListener('click', () => {
-            mobileMenu.classList.remove('mobile-menu-open');
-        });
+const menuToggle = document.getElementById('menuToggle');
+const mobileMenu = document.getElementById('mobileMenu');
+
+menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
+
+// Close menu when clicking a link
+document.querySelectorAll('#mobileMenu a').forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.add('hidden');
     });
+});
